19f3678855253885a6007acdaac22e1f89664b8f
emeath <matheus.martins.jeronimo@gmail.com>
2022-08-10 17:02:35 -0300

    First commit

---
+++ src/main/java/com/banco/conta_segura/ContaSeguraApplication.java
@@ -0,0 +1,13 @@
+package com.banco.conta_segura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaSeguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaSeguraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banco/conta_segura/ContaSeguraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.conta_segura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaSeguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
